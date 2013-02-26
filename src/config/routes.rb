27ce5ae2d23c5ec99b922f49662525de68944606
Tyler Copple <j.tylercopple@gmail.com>
2013-02-25 23:19:57 -0500

    Added form to justin's page, doesn't actually email yet.

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,5 +3,6 @@ HolyfireMinistries::Application.routes.draw do
   # resources :pages
   # match ":permalink" => "pages#show", as: :permalink
   match "/justin-kendrick" => "pages#kendrick"
+  match "/email" => "pages#email"
   root to: "pages#index"
 end
