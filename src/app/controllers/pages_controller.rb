a1bd8a0eff1d875318853409c24d176a7066c071
Tyler Copple <j.tylercopple@gmail.com>
2013-03-02 16:54:08 -0500

    Adding copy to JK's page, also forgot to add mailer controller action.

--- src/app/controllers/pages_controller.rb
+++ src/app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
   end
 
   def email
+    ContactMailer.send_contact_email(params).deliver
     @message = "Thank you for your request, someone should respond within 24 hours."
     render 'kendrick', layout: 'kendrick'
   end
