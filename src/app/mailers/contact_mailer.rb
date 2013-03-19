e70e959f210bdfc6b91cafcb6d3f8ff643cb759d
User account to facilitate holyfire ministires test website <holyfireministries-s@abednego.(none)>
2013-03-19 01:26:26 -0400

    Setup for production information of contactmailer using sendmail.

--- src/app/mailers/contact_mailer.rb
+++ src/app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
   default from: "holyfireministries-website@holyfireministries.com",
-          to: ["j.tylercopple@gmail.com", "stephanie@holyfireministries.com"]
+          to: ["j.tylercopple@gmail.com"]#, "stephanie@holyfireministries.com"]
 
   def send_contact_email(info)
     @phone = info[:phone]
